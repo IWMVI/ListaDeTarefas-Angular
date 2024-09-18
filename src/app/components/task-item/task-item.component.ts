621c719710123b3862db9ec8281d9fb363b3f483
Wallace Martins <wmvwallace@gmail.com>
2024-09-18 13:47:04 -0300

    Components: Header, Tasks, Task-Item. Services, banco de dados

---
+++ src/app/components/task-item/task-item.component.ts
@@ -0,0 +1,23 @@
+import { Component, EventEmitter, Input, input, Output } from '@angular/core';
+import { Tarefa } from '../../../Tarefa';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-task-item',
+  standalone: true,
+  imports: [FontAwesomeModule, CommonModule],
+  templateUrl: './task-item.component.html',
+  styleUrl: './task-item.component.scss',
+})
+export class TaskItemComponent {
+  @Input() tarefa!: Tarefa;
+  @Output() onDeleteTask = new EventEmitter<Tarefa>();
+
+  onDelete(tarefa: Tarefa) {
+    this.onDeleteTask.emit(tarefa);
+  }
+
+  faTimes = faTimes;
+}
